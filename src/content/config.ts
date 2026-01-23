015dad7d67d801d75d19b1eefd8654d9c95e85ea
Anish Govind <anishg24@gmail.com>
2026-01-22 17:55:39 -0800

    added env variables from cloudflare

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,11 +3,11 @@ import { r2Loader } from "../loaders/r2-loader";
 
 const photos = defineCollection({
     loader: r2Loader({
-        bucket: import.meta.env.R2_BUCKET_NAME,
-        endpoint: import.meta.env.R2_ENDPOINT,
-        accessKeyId: import.meta.env.R2_ACCESS_KEY_ID,
-        secretAccessKey: import.meta.env.R2_SECRET_ACCESS_KEY,
-        publicDomain: import.meta.env.R2_PUBLIC_DOMAIN,
+        bucket: (import.meta.env.R2_BUCKET_NAME || process.env.R2_BUCKET_NAME) ?? "",
+        endpoint: (import.meta.env.R2_ENDPOINT || process.env.R2_ENDPOINT) ?? "",
+        accessKeyId: (import.meta.env.R2_ACCESS_KEY_ID || process.env.R2_ACCESS_KEY_ID) ?? "",
+        secretAccessKey: (import.meta.env.R2_SECRET_ACCESS_KEY || process.env.R2_SECRET_ACCESS_KEY) ?? "",
+        publicDomain: (import.meta.env.R2_PUBLIC_DOMAIN || process.env.R2_PUBLIC_DOMAIN) ?? "",
     }),
 });
 
